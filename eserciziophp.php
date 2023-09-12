28133d4ac85fcaff750903e5a544d099309eb2f5
EmanueleZago <lelepoci@gmail.com>
2023-09-12 20:54:15 +0200

    first commit

---
+++ eserciziophp.php
@@ -0,0 +1,79 @@
+<?php
+//ESERCIZIO 1
+
+$integer = 10;
+$float = 5.5;
+$string = "ciao";
+$boolean = true;
+
+var_dump($integer,$float,$string,$boolean);
+
+
+define('integer', 10);
+define('float', 5.5);
+define('string', "ciao");
+define('boolean', true);
+
+//ESERCIZIO 2
+
+
+$text = "Marco "; 
+$text2 = "hai "; 
+$text3 = "sete "; 
+$text4 = "? "; 
+$text5 = "Perchè "; 
+$text6 = $text2; 
+$text7 = "bevuto "; 
+$text8 = "tutto ";
+$testo = $text . $text2 . $text3 . $text4 . $text5 . $text6 . $text7 . $text8;
+
+var_dump($testo);
+
+//ESERCIZIO 3
+
+
+$words1 = [ 
+    'una', 
+    67, 
+    'vita', 
+    'colle',
+    'mi', 
+    'rosso', 
+    [ 
+      'oscura', 
+      'era', 
+      89, 
+      [ 
+        'mezzo',
+        [ 
+          'cammin', 
+          'Nel', 
+          [ 
+            'selva', 
+            'la', 
+            [ 
+              'via', 
+              'una', 
+              true, 
+            ] 
+          ], 
+        ] 
+      ], 
+    'ritrovai', 
+    'per' 
+    ], 
+    'diritta' 
+  ]; 
+  $words2 = [ 
+    'elemento1' => 25.89, 
+    'elemento2' => 'nostra', 
+    'elemento3' => [ 
+                      'Virgilio', 
+                      'smarrita', 
+                      'ché' 
+                    ] 
+  ];
+
+  $result = $words1[6][3][1][1] $worlds1[6][3][1][0]; 
+
+  echo $result;
