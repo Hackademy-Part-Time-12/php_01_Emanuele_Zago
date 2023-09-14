060285755f06ead1ccdb4faee76fb9508f2469a3
EmanueleZago <lelepoci@gmail.com>
2023-09-14 19:48:13 +0200

    Inizio secondo esercizio

--- Esercizio02.php
+++ Esercizio02.php
@@ -2,3 +2,21 @@
 
 // Esercizio 1
 
+$numeriScelta = [85,4,16,84,1,68,41,89,6,43,86];
+$numeriGiusti = 0;
+$somma = 0;
+$media = 0;
+
+
+foreach($numeriScelta as $multipli){
+    if($multipli % 3 == 0){
+    $somma += $multipli;
+    $numeriGiusti++;
+    $media = $somma / $numeriGiusti;
+    }
+};
+
+echo"la media risulta $media";
+
+// Esercizio 2
+
